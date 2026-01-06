84c2993837b195094df1d5135153543a45482eb6
Sasha <sashabaratow@gmail.com>
2026-01-07 01:27:03 +0600

    auth using clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import {Button} from "@/components/ui/button";
+import {UserButton} from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
         <h1>test </h1>
         <Button> test</Button>
+        <UserButton/>
     </div>
   );
 }
---
+++ app/provider.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {UserDetailContext} from "@/context/UserDetailContext";
+
+function Provider({children}) {
+
+    const [userState, setUserState] = useState()
+
+    const createNewUser = async () => {
+        const result = await axios.post('/api/user', {})
+
+        console.log(result?.data)
+        setUserState(result.data)
+    }
+
+    useEffect(() => {
+        createNewUser()
+    }, [])
+
+
+
+    return (
+        <UserDetailContext value={{userState, setUserState}}>
+            <div>{children}</div>
+        </UserDetailContext>
+    );
+}
+
+export default Provider;
