71227af042570cccf7ec84ba6ec0963be4af99ff
Sasha <sashabaratow@gmail.com>
2026-01-07 21:26:36 +0600

    Landing screen & project saving to db

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-import {Button} from "@/components/ui/button";
-import {UserButton} from "@clerk/nextjs";
+import Header from "@/app/_shared/Header";
+import Hero from "@/app/_shared/hero/Hero";
 
 export default function Home() {
   return (
-    <div>
-        <h1>test </h1>
-        <Button> test</Button>
-        <UserButton/>
+    <div className={'w-full px-2 py-1'}>
+        <Header/>
+        <Hero/>
+        <div className={'absolute -top-40 -left-40 h-[500px] w-[500px] bg-purple-400/20 blur-[120px] rounded-full z-0'}/>
+        <div className={'absolute -top-40 -right-0 h-[500px] w-[500px] bg-pink-400/20 blur-[120px] rounded-full z-0'}/>
+        <div className={'absolute -bottom-40 left-1/5 h-[500px] w-[500px] bg-blue-400/20 blur-[120px] rounded-full z-0'}/>
+        <div className={'absolute -bottom-40 -right-[-200px] h-[500px] w-[500px] bg-sky-400/20 blur-[120px] rounded-full z-0'}/>
     </div>
   );
 }
