071c5a5afa73dd6e619c0585214e92e75dbd81c5
Sasha <sashabaratow@gmail.com>
2026-01-12 00:41:54 +0600

    generate screen mock UIUX

--- app/project/[projectId]/page.tsx
+++ app/project/[projectId]/page.tsx
@@ -15,6 +15,7 @@ function ProjectCanvasPlayground() {
     const [projectDetails, setProjectDetails] = useState<ProjectType>()
     const [screenConfig, setScreenConfig] = useState<ScreenConfigType[]>()
     const [loading, setLoading] = useState<boolean>(false)
+    const [loadingMsg, setLoadingMsg] = useState<string>('Loading')
 
 
     const getProjectDetails = async () => {
@@ -27,6 +28,7 @@ function ProjectCanvasPlayground() {
 
     const generateScreenConfig = async () => {
         setLoading(true)
+        setLoadingMsg('Generating screen...')
         const result = await axios.post('/api/generate-config', {
             projectId: projectId,
             deviceType: projectDetails?.device,
@@ -37,6 +39,30 @@ function ProjectCanvasPlayground() {
         setLoading(false)
     }
 
+    const generateScreenUiUx = async () => {
+        setLoading(true)
+        if (screenConfig && Array.isArray(screenConfig)) {
+            for (let i = 0; i < screenConfig?.length; i++) {
+                const screen = screenConfig[i]
+                if (screen?.code) continue
+                setLoadingMsg('Generating screen...' + ' ' + (i+1))
+
+                const result = await axios.post(`/api/generate-screen-ui`, {
+                    projectId,
+                    screenId: screen.id,
+                    screenName: screen?.screenName,
+                    purpose: screen.purpose,
+                    screenDescription: screen.screenDescription,
+                })
+
+                setScreenConfig( ( prev ) => prev?.map(( item, index) =>
+                ( index === i ? result?.data : item)))
+
+            }
+        }
+        setLoading(false)
+    }
+
     useEffect(() => {
         getProjectDetails()
     }, [projectId])
@@ -44,9 +70,14 @@ function ProjectCanvasPlayground() {
     useEffect(() => {
         if (projectDetails && screenConfig && screenConfig?.length === 0) {
             generateScreenConfig()
+        }else if (screenConfig && projectDetails) {
+            generateScreenUiUx()
         }
 
     }, [projectDetails, screenConfig])
+
+
+
 
     return (
         <div className={'w-full px-2 py-3'}>
@@ -54,7 +85,8 @@ function ProjectCanvasPlayground() {
             <div className={''}>
                 {loading &&
                     <div className={'p-3 absolute left-1/2 top-20 bg-blue-300/20 border-b-blue-600 rounded-xl'}>
-                        <h2 className={'flex gap-2 items-center text-primary'}><Loader2Icon className={'animate-spin'}/> Loading...</h2>
+                        <h2 className={'flex gap-2 items-center text-primary'}><Loader2Icon
+                            className={'animate-spin'}/> {loadingMsg}</h2>
                     </div>
                 }
                 <SettingSection projectDetails={projectDetails}/>
